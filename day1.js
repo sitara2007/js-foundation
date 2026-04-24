cd58749706381d4845c19108ec605554c4489914
sitara2007 <2510001409@cgcuniversity.org>
2026-04-25 03:00:15 +0530

    Add basic arithmetic operations in day1.js

---
+++ day1.js
@@ -0,0 +1,9 @@
+let a = 2;
+let b = 3;
+
+console.log("Sum:", a + b);
+console.log("Difference:", a - b);
+console.log("Product:", a * b);
+console.log("Quotient:", a / b);
+console.log("Remainder:", a % b);
+console.log("Power:", a ** b);
